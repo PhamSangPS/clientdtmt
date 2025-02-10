239bd47771945598a452c3c0133e02d61b279019
PhamSangPS <160474426+PhamSangPS@users.noreply.github.com>
2025-02-10 23:21:31 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -91,65 +91,65 @@ export default function Footer() {
                 <path
                   d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
               </svg>
-              TW Elements
+              MT HEATER
             </h6>
             <p>
-              Here you can use rows and columns to organize your footer
-              content. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit.
+              Chúng tôi chuyên cung cấp điện trở công nghiệp công suất
+              lớn. Chất lượng được đặt lên hàng đầu, tư vấn kỹ thuật
+              và bảo hành lâu dài.
             </p>
           </div>
           {/* <!-- Products section --> */}
           <div className="">
             <h6
               className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Products
+              Sản Phẩm
             </h6>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Angular</a>
+              >Điện Trở </a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >React</a>
+              >Phụ Kiện</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Vue</a>
+              >Tủ Điện Điều Khiển</a>
             </p>
             <p>
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Laravel</a>
+              >Vòng Trở</a>
             </p>
           </div>
-          {/* <!-- Useful links section --> */}
+          {/* <!-- Useful links sectio --> */}
           <div className="">
             <h6
               className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Useful links
+              Dịch Vụ
             </h6>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Pricing</a>
+              >Đặt Hàng Theo Yêu Cầu</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Settings</a>
+              >Tư Vấn Kỹ Thuật</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Orders</a>
+              >Bảo Hành Lâu Dài</a>
             </p>
             <p>
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Help</a>
+              >Sữa Chữa Tại Nơi</a>
             </p>
           </div>
           {/* <!-- Contact section --> */}
           <div>
             <h6
               className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Contact
+              Liên Hệ
             </h6>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -162,7 +162,7 @@ export default function Footer() {
                 <path
                   d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
               </svg>
-              New York, NY 10012, US
+              6/9 Đỗ Văn Dậy, Xã Tân Hiệp, Huyện Hóc Môn, TP.HCM
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -175,7 +175,7 @@ export default function Footer() {
                 <path
                   d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
               </svg>
-              info@example.com
+              dientromt@gmail.com
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -201,7 +201,7 @@ export default function Footer() {
                   d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
                   clipRule="evenodd" />
               </svg>
-              + 01 234 567 89
+              0352627623
             </p>
           </div>
         </div>
@@ -209,11 +209,11 @@ export default function Footer() {
 
       {/* <!--Copyright section--> */}
       <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
-        <span>© 2023 Copyright:</span>
+        <span>© 2025 Đăng Ký</span>
         <a
           className="font-semibold text-neutral-600 dark:text-neutral-400"
-          href="https://tw-elements.com/"
-        >TW Elements</a>
+         
+        >Bản Quyền</a>
       </div>
     </footer>
   );
